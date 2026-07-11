4626e1f3e174e2af90939cd2ca3f7d5dca3484b8
019-Mahansa <lightningplay2009@gmail.com>
2026-07-11 09:02:44 +0700

    fix button dan image to same

--- src/components/CardDosen.jsx
+++ src/components/CardDosen.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import fotoDosen from '../assets/1.png'
+import { getFotoDosen } from '../data/fotoMap'
 import './CardDosen.css'
 
 export default function CardDosen({ dosen, lang }) {
   const isEn = lang === 'en'
+  const foto = getFotoDosen(dosen.id)
 
   return (
     <div className="card-dosen">
-      <img src={fotoDosen} alt={dosen.nama} className="foto-dosen" />
+      <img src={foto} alt={dosen.nama} className="foto-dosen" />
       
       <div className="card-header">
         <h3>{dosen.nama}</h3>
@@ -35,14 +36,16 @@ export default function CardDosen({ dosen, lang }) {
         </div>
         
         {dosen.pddikti && (
-          <a 
-            href={dosen.pddikti} 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            className="link-pddikti"
-          >
-            PDDikti
-          </a>
+          <div className="pddikti-wrapper">
+            <a 
+              href={dosen.pddikti} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="link-pddikti"
+            >
+              PDDikti
+            </a>
+          </div>
         )}
       </div>
     </div>
---
+++ src/data/fotoMap.js
@@ -0,0 +1,72 @@
+// Mapping foto dosen. Jika tidak ada, fallback ke icon.jpeg.
+// Import gambar di sini agar Vite bisa membundle dan hashing nama file.
+import icon from "../assets/imageDosen/icon.jpeg";
+import ichwani from "../assets/1.png";
+import sugiyanto from "../assets/imageDosen/Sugiyanto.jpg";
+import roni from "../assets/imageDosen/Roni_Rustandi.jpg";
+import herdi from "../assets/imageDosen/herdi_wisman_jaya.jpg";
+import ahmadNana from "../assets/imageDosen/Ahmad_Nana_Mahmur.jpg";
+import dedeSiswandi from "../assets/imageDosen/R_dede_Siswandi.jpg";
+import setiawati from "../assets/imageDosen/Setiawati.jpg";
+import masFierma from "../assets/imageDosen/Mas_Fierna_Janvierna.jpg";
+import yatti from "../assets/imageDosen/Yatti_rosmiati.jpg";
+import dewiPurnama from "../assets/imageDosen/Dewi_Purnama_seri.jpg";
+import heriKurnia from "../assets/imageDosen/Heri_Kurnia.jpg";
+import endang from "../assets/imageDosen/Endang_Prastini.jpg";
+import abiRobian from "../assets/imageDosen/Abi Robian.jpg";
+import dini from "../assets/imageDosen/Dini_handayani.jpg";
+import saepudin from "../assets/imageDosen/Saepudin_Karta_Sasmita.jpg";
+import lathifah from "../assets/imageDosen/Lathifah_Sandra.jpg";
+import raistin from "../assets/imageDosen/Raistin_Nur_Abidin.jpg";
+import susi from "../assets/imageDosen/Susi.jpg";
+import suheppy from "../assets/imageDosen/Suhappy.jpg";
+import nurullita from "../assets/imageDosen/Nurrullita.jpg";
+import yayuk from "../assets/imageDosen/Yayuk_Muji_Rahayu.jpg";
+import ivana from "../assets/imageDosen/Ivana_Aprilliana-.jpg";
+import suanto from "../assets/imageDosen/Suanto.jpg";
+
+// key = id dosen, value = import foto
+const fotoMap = {
+  1: ichwani, // Ichwani -> 1.png
+  2: sugiyanto,
+  3: roni,
+  4: icon, // Aulia -> icon
+  5: icon, // Nurdiyana -> icon
+  6: herdi,
+  7: icon, // Eti Hayati -> icon
+  8: icon, // Abd. Chaidir -> icon
+  9: ahmadNana,
+  10: dedeSiswandi,
+  11: setiawati,
+  12: masFierma,
+  13: icon, // Hendri -> icon
+  14: yatti,
+  15: dewiPurnama,
+  16: heriKurnia,
+  17: icon, // Awang Muhidin -> icon
+  18: endang,
+  19: abiRobian,
+  20: icon, // Amrizal -> icon
+  21: dini,
+  22: saepudin,
+  23: icon, // Darto -> icon
+  24: lathifah,
+  25: icon, // Eka Setiana -> icon
+  26: raistin,
+  27: susi,
+  28: suheppy,
+  29: nurullita,
+  30: yayuk,
+  31: icon, // Ruli Hardi -> icon
+  32: icon, // M. Finandar -> icon
+  33: ivana,
+  34: icon, // Supandri -> icon
+  35: suanto,
+  36: icon, // Sulastri -> icon
+};
+
+export function getFotoDosen(id) {
+  return fotoMap[id] || icon;
+}
+
+export default fotoMap;
