3d2590c2faa7faf43d2de7d7d5e5a3d18b7638b2
git <aldiansyahreza2009@gmail.com>
2026-07-17 20:44:14 +0700

    test resposif

--- src/pages/DaftarDosen.jsx
+++ src/pages/DaftarDosen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams, Navigate } from "react-router-dom";
 import { dataDosenID } from "../data/dosen-id";
 import { dataDosenEN } from "../data/dosen-en";
@@ -6,23 +6,47 @@ import CardDosen from "../components/CardDosen";
 
 function DaftarDosen() {
   const { lang } = useParams();
+  const containerRef = useRef(null);
 
   if (lang !== "id" && lang !== "en") {
     return <Navigate to="/dosen/id" replace />;
   }
 
   const currentData = lang === "id" ? dataDosenID : dataDosenEN;
-  // const title =
-  //   lang === "id"
-  //     ? "Daftar Dosen Prodi PPKn"
-  //     : "Civics Education Lecturers List";
+
+  useEffect(() => {
+    const sendHeight = () => {
+      const height = document.documentElement.scrollHeight;
+
+      window.parent.postMessage(
+        {
+          type: "resize-iframe",
+          height: height,
+        },
+        "*",
+      );
+    };
+
+    const observer = new ResizeObserver(() => {
+      sendHeight();
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    sendHeight();
+
+    window.addEventListener("resize", sendHeight);
+
+    return () => {
+      observer.disconnect();
+      window.removeEventListener("resize", sendHeight);
+    };
+  }, []);
 
   return (
-    <div className="app-container">
-      {/* <h1 className="title" style={{ textAlign: "center", margin: "2rem 0" }}>
-        {title}
-      </h1> */}
-
+    <div className="app-container" ref={containerRef}>
       <div className="grid-dosen">
         {currentData.map((dosen) => (
           <CardDosen key={dosen.id} dosen={dosen} lang={lang} />
