c23741f6d31e66be5aebce7acd6ee62cb373ea92
019-Mahansa <lightningplay2009@gmail.com>
2026-07-24 10:28:28 +0700

    add tab

--- src/pages/daftarDosen/DaftarDosen.jsx
+++ src/pages/daftarDosen/DaftarDosen.jsx
@@ -7,15 +7,15 @@ import { useEffect } from 'react';
 
 function DaftarDosen() {
   
-useEffect(() => {
-  // Setelah render, kirim tinggi
-  const kirim = () => {
-    window.parent.postMessage({ tinggi: document.documentElement.scrollHeight }, '*');
-  };
-  kirim();
-  window.addEventListener('resize', kirim);
-  return () => window.removeEventListener('resize', kirim);
-}, [data]); // kalau data adalah state yang sudah terisi
+  useEffect(() => {
+    // Setelah render, kirim tinggi
+    const kirim = () => {
+      window.parent.postMessage({ tinggi: document.documentElement.scrollHeight }, '*');
+    };
+    kirim();
+    window.addEventListener('resize', kirim);
+    return () => window.removeEventListener('resize', kirim);
+  }, [data]); // kalau data adalah state yang sudah terisi
 
   const { lang } = useParams();
 
